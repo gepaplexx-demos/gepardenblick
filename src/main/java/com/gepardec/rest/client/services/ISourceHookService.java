be5d4fd8f41ecacbf6019f948566f4c2015b731f
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2022-07-18 11:08:11 +0200

    Trigger workflow

--- src/main/java/com/gepardec/rest/client/services/ISourceHookService.java
+++ src/main/java/com/gepardec/rest/client/services/ISourceHookService.java
@@ -7,8 +7,10 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-import javax.ws.rs.*;
-import java.util.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.List;
+import java.util.Set;
 
 @Path("/repos")
 @RegisterRestClient
