8e67277ed9c5b3a4f4d2cc5948a91e522c8d68b5
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2022-07-19 09:19:25 +0200

    imports vereinfachen

--- src/main/java/com/gepardec/rest/client/resources/RepositoryResource.java
+++ src/main/java/com/gepardec/rest/client/resources/RepositoryResource.java
@@ -6,10 +6,8 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import java.util.HashMap;
-import java.util.HashSet;
+import javax.ws.rs.*;
+import java.util.*;
 
 @Path("/repo")
 public class RepositoryResource {
