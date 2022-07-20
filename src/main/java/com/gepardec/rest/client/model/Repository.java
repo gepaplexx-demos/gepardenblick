afc5b8dc7d71de06280fe4d83d461341fa0e7b49
ThomasKianek-Gepardec <thomas.kianek@gepardec.com>
2022-07-20 11:09:12 +0200

    Added first Mockito test for the repository resource

--- src/main/java/com/gepardec/rest/client/model/Repository.java
+++ src/main/java/com/gepardec/rest/client/model/Repository.java
@@ -3,10 +3,16 @@ package com.gepardec.rest.client.model;
 import java.util.HashMap;
 
 public class Repository {
+
     public String name;
     public String html_url;
-
     public HashMap<String, String> permissions;
+
+    public Repository(String name, String html_url, HashMap<String, String> permissions) {
+        this.name = name;
+        this.html_url = html_url;
+        this.permissions = permissions;
+    }
 }
 
 
---
+++ src/test/java/com/gepardec/rest/client/resources/RepositoryResourceTests.java
@@ -0,0 +1,55 @@
+package com.gepardec.rest.client.resources;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gepardec.rest.client.model.Repository;
+import com.gepardec.rest.client.services.IRepositoryService;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.HashSet;
+
+@QuarkusTest
+public class RepositoryResourceTests {
+
+    private final String TEST_ORG = "testorg";
+
+    @InjectMock(convertScopes = true)
+    @RestClient
+    IRepositoryService repositoryService;
+
+    @Inject
+    RepositoryResource repositoryResource;
+
+    @Inject
+    ObjectMapper mapper;
+
+    private HashSet<Repository> testRepos;
+    private HashMap<String, String> testPermissions;
+    private HashMap<String, String> expectedResponse;
+
+    @BeforeEach
+    public void setup() {
+        testRepos = new HashSet<>();
+        testPermissions = new HashMap<>();
+        expectedResponse = new HashMap<>();
+    }
+
+    @Test
+    public void whenGetReposByOrgGivenOrgName_thenReturnValidJsonOfSingleRepo() throws JsonProcessingException {
+        testPermissions.put("admin", "true");
+        Repository repository = new Repository("repo1", "https://github.com/repo1", testPermissions);
+        testRepos.add(repository);
+        expectedResponse.put(repository.name, repository.html_url);
+        Mockito.when(repositoryService.getReposByOrg(TEST_ORG)).thenReturn(testRepos);
+
+        Assertions.assertEquals(repositoryResource.getReposByOrg(TEST_ORG), mapper.writeValueAsString(expectedResponse));
+    }
+}
